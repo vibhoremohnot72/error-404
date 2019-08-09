3c63f908705402785ebbf19121c53a13d2a0c35f
Vibhore Mohnot <45279814+vibhoremohnot72@users.noreply.github.com>
2019-08-09 09:02:01 +0530

    commit

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,1 +1,9 @@
-$('#navbar').bootnavbar();
+$(".btn-group, .dropdown").hover(
+    function () {
+        $('>.dropdown-menu', this).stop(true, true).fadeIn("fast");
+        $(this).addClass('open');
+    },
+    function () {
+        $('>.dropdown-menu', this).stop(true, true).fadeOut("fast");
+        $(this).removeClass('open');
+    });
